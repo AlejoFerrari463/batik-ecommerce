4d39f71721e75f1ceae822d7641d475f1cdf0584
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-06-05 14:13:45 -0300

    funcion de deshabilitar todos los checkbox menos el que se selecciona hecho (solo para categorias)

--- javascript/hombres.js
+++ javascript/hombres.js
@@ -1,32 +1,71 @@
 import * as c from "../javascript/cargarProductos.js"
 
-c.cargaDeProductos("Hombre")
+let h = "Hombre"
+
+c.cargaDeProductos(h)
 
 
 const filtro = document.querySelectorAll("input")
 
 
+function desabilitarCheckBox(exep){
+
+    filtro.forEach((element)=>{
+
+        if (element.value!=exep){
+            element.disabled = true
+        }
+        
+        
+        
+    })
+
+}
+
+function habilitarCheckBox(exep){
+
+    filtro.forEach((element)=>{
+
+       
+        element.disabled = false
+   
+        
+        
+        
+    })
+
+}
+
 
 filtro.forEach((element)=>{
 
+    
+
     element.addEventListener("click",(event)=>{
         
+        console.log(element.tabIndex)
     
         if (element.checked){
+
+            desabilitarCheckBox(event.target.value);
     
-        c.cargaDeProductosPorCategoria("Hombre",event.target.value)
-    
-    
+            c.cargaDeProductosPorCategoria(h,event.target.value)
+          
+          
         }
     
         else {
-            c.cargaDeProductos("Hombre")
+            c.cargaDeProductos(h)
+
+            habilitarCheckBox()
+            
         }
     
+                
        
     })
 
-    
 
 })
+
 
